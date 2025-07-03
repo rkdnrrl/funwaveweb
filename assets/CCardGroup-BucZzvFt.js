be14e04830c6f46693e4f31ddf4b78b2496e95d6
kgu <rkdnrr1@gmail.com>
2025-07-03 18:54:22 +0900

    Updates

---
+++ assets/CCardGroup-BucZzvFt.js
@@ -0,0 +1,1 @@
+import{r as o,_ as p,R as t,d as n,e as i,P as r}from"./index-DjdDtEwC.js";var s=o.forwardRef(function(a,e){var c=a.children,d=a.className,l=p(a,["children","className"]);return t.createElement("div",n({className:i("card-group",d)},l,{ref:e}),c)});s.propTypes={children:r.node,className:r.string};s.displayName="CCardGroup";export{s as C};
